30774c8e97b763cb94e2976c12ff625deefc84f8
zhukovsky <zhukovsky@users.noreply.github.com>
2020-10-29 22:08:57 +0300

    Update autocontrast.py

--- autocontrast.py
+++ autocontrast.py
@@ -1,19 +1,24 @@
-from __future__ import print_function
-from sys import argv
-import os.path
+import sys, os.path, cv2, numpy as np
 
 
-def autocontrast(src_path, dst_path, white_perc, black_perc):
-    pass
+def autocontrast(img: np.ndarray, white_percent: float, black_percent: float) -> np.ndarray:
+    pass  # insert your code here
+
+
+def main():
+    assert len(sys.argv) == 5
+    src_path, dst_path = sys.argv[1], sys.argv[2]
+    white_percent, black_percent = float(sys.argv[3]), float(sys.argv[4])
+    assert 0 <= white_percent < 1
+    assert 0 <= black_percent < 1
+
+    assert os.path.exists(src_path)
+    img = cv2.imread(src_path, cv2.IMREAD_GRAYSCALE)
+    assert img is not None
+
+    result = autocontrast(img, white_percent, black_percent)
+    cv2.imwrite(dst_path, result)
 
 
 if __name__ == '__main__':
-    assert len(argv) == 5
-    assert os.path.exists(argv[1])
-    argv[3] = float(argv[3])
-    argv[4] = float(argv[4])
-
-    assert 0 <= argv[3] < 1
-    assert 0 <= argv[4] < 1
-
-    autocontrast(*argv[1:])
+    main()
